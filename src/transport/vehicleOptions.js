5f542e0a7a5cc51be396de578f1471142be8466b
nitroglicerine <konyaka321@gmail.com>
2023-11-04 22:12:49 +0200

    new modules with task

---
+++ src/transport/vehicleOptions.js
@@ -0,0 +1,14 @@
+
+class Vehicle {
+    constructor(isDamaged, productYear) {
+      (this.isDamaged = isDamaged), (this.productYear = productYear);
+    }
+    setDamage() {
+      this.isDamaged = confirm("Has damaged ?");
+    }
+    yearSet() {
+      this.productYear = +prompt("insert year ", ["1984"]);
+    }
+  }
+
+  export{Vehicle}
