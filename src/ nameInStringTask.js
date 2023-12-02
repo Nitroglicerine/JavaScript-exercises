660b257d22048565dd64246eafd448c38dd9425b
nitroglicerine <konyaka321@gmail.com>
2023-12-02 12:46:46 +0200

    next task is done

---
+++ src/ nameInStringTask.js
@@ -0,0 +1,17 @@
+export function nameInStr(arg1, arg2) {
+  let arr1 = [];
+  let test;
+  for (let i = 0; i < arg2.length; i++) {
+    test = arg1.indexOf(arg2[i], test);
+    if (test !== -1) {
+      arr1.push(test);
+    }
+  }
+
+  if (arr1.length != arg2.length) {
+    return false;
+  }
+  return true;
+}
+
+console.log(nameInStr("Across the rivers", "chris"));
